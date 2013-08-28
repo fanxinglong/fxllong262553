66a2bb0caaa16c471c5aeac32fc9645740c46da7
fanxinglong <fanxinglong@tigerjoys.com>
2013-08-28 14:19:36 +0800

    添加主题

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/fxllong262553/"
+http_images_path = "/fxllong262553/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/fxllong262553/fonts"
+css_dir = "public/fxllong262553/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
---
+++ source/_posts/2013-08-27-hello-world.markdown
@@ -0,0 +1,7 @@
+---
+layout: post
+title: "hello world"
+date: 2013-08-27 17:01
+comments: true
+categories: 
+---
